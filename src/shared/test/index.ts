957e41bb9a21d9c312e8ace09122a367c0a58efa
KennyR <kenny.ramin433@gmail.com>
2026-04-29 23:55:52 +0200

    :wrench: chore: add Vitest configuration + test setup

---
+++ src/shared/test/index.ts
@@ -0,0 +1,3 @@
+export { render } from '@testing-library/react'
+export { default as userEvent } from '@testing-library/user-event'
+export { renderHook, act } from '@testing-library/react'
---
+++ vitest.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vitest/config'
+import { resolve } from 'path'
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/shared/test/setup.ts'],
+    include: ['src/**/*.test.{ts,tsx}'],
+    alias: {
+      '@student': resolve('src/features/student/renderer'),
+      '@frequentation': resolve('src/features/frequentation/renderer'),
+      '@student-shared': resolve('src/features/student/shared'),
+      '@frequentation-shared': resolve('src/features/frequentation/shared'),
+      '@shared': resolve('src/shared'),
+      '@ui': resolve('src/shared/ui'),
+      '@lib': resolve('src/shared/lib'),
+      '@types': resolve('src/shared/types')
+    }
+  }
+})
