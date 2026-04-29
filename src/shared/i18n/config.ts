abf8f98844b8b8c171a5b07a0395402da2c4c1eb
KennyR <kenny.ramin433@gmail.com>
2026-04-29 23:58:08 +0200

    :sparkles: feat: add i18n configuration with French locales

---
+++ src/shared/i18n/config.ts
@@ -0,0 +1,24 @@
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+
+import commonFr from './locales/fr/common.json'
+import studentFr from './locales/fr/student.json'
+import frequentationFr from './locales/fr/frequentation.json'
+
+export const defaultNS = 'common'
+
+i18n.use(initReactI18next).init({
+  resources: {
+    fr: {
+      common: commonFr,
+      student: studentFr,
+      frequentation: frequentationFr
+    }
+  },
+  lng: 'fr',
+  fallbackLng: 'fr',
+  defaultNS,
+  interpolation: { escapeValue: false }
+})
+
+export default i18n
