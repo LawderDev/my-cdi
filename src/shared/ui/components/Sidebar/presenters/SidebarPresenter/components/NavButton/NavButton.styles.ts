f3a2a9bcd7426146a412ff4cd18e2e865d38b74e
KennyR <kenny.ramin433@gmail.com>
2026-09-01 20:15:44 +0200

    :bug: fix(ui): restore nav icon size on TYPE_SCALE h5 step

--- src/shared/ui/components/Sidebar/presenters/SidebarPresenter/components/NavButton/NavButton.styles.ts
+++ src/shared/ui/components/Sidebar/presenters/SidebarPresenter/components/NavButton/NavButton.styles.ts
@@ -1,6 +1,6 @@
 import { alpha, styled } from '@mui/material/styles'
 import { shouldForwardStyledProp } from '@ui/helpers/shouldForwardStyledProp'
-import { TINT_ALPHAS, theme } from '@ui/theme'
+import { TINT_ALPHAS, TYPE_SCALE } from '@ui/theme'
 
 const NAV_BTN_WIDTH_PX = 52
 const NAV_BTN_HEIGHT_PX = 48
@@ -9,7 +9,7 @@ const ACTIVE_BAR_HEIGHT_PX = 20
 const ACTIVE_BAR_LEFT_PX = -8
 const ACTIVE_BAR_BORDER_RADIUS = '0 3px 3px 0'
 
-export const ICON_FONT_SIZE_PX = theme.typography.subtitle1.fontSize
+export const ICON_FONT_SIZE_PX = TYPE_SCALE.h5
 
 export const NavButtonRoot = styled('button', {
   shouldForwardProp: shouldForwardStyledProp
